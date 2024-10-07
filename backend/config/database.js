74d23281c72f4f4a0de9719eb1d768700e9e5834
palkingshuk <pkingshuk35@gmail.com>
2024-10-07 21:29:52 +0530

    backend

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+const databaseConnection = () => {
+    mongoose.connect(process.env.MONGO_URL).then(()=>{
+        console.log("Connected to mongoDB");
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
+export default databaseConnection;
